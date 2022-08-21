b40ae49051b50ef8d225651076c416b8415ea2ae
Maureen0007 <maureen.wanjiku@student.moringaschool.com>
2022-08-21 11:09:10 +0300

    commit tree 9d288191b128dc2804c753e0a03503929a3c9211

--- methods.rb
+++ methods.rb
@@ -1,1 +1,38 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+    
+end
+greet_programmer
+
+def greet(name = "Naureen")
+    puts "Hello, #{name}!"
+    
+end
+greet "Jimmy"
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+    
+end
+
+greet_with_default "Naureen"
+
+def add num1, num2
+    return num1+ num2
+
+end
+
+add 2,2
+
+
+def halve (number)
+    if number.class != Integer
+        return nil
+    
+end
+
+return number / 2
+
+end
